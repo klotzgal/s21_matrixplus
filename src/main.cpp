61ad5d584d7aa25aad15788db99032e266c09101
klotzgal <ggg44466635@gmail.com>
2023-09-24 13:56:22 +0300

    INIT

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "s21_matrix_oop.h"
+
+int main() {
+  std::cout << "Matrix"
+            << "\n";
+  return 0;
+}
---
+++ src/s21_matrix_oop.h
@@ -0,0 +1,7 @@
+#ifndef _S21_MATRIX_OOP_H_
+#define _S21_MATRIX_OOP_H_
+
+#include <iostream>
+
+
+#endif  // _S21_MATRIX_OOP_H_
